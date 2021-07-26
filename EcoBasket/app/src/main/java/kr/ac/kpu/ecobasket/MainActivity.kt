65773d4cea553a2503ba3c0b9eac44fdf0f38720
chanho <ncclub12@naver.com>
2021-07-26 19:06:29 +0900

    Chanho Muyaho

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // Jaehyun Commit Test
         // Jinho Commit Test 6:51
+        // Muyaho
     }
 }
