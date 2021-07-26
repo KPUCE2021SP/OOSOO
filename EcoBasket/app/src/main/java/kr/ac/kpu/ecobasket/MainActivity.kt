104966f6905a3e7edd78c23c4fd7b9e3d4df704e
Jinho <wlsgh980118@gmail.com>
2021-07-26 18:51:39 +0900

    jino 6:51

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Jaehyun Commit Test
+        // Jinho Commit Test 6:51
     }
 }
