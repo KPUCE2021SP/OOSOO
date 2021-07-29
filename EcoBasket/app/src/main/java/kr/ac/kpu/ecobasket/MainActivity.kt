18a445514eacf3d339635ad5a9143bd1cf5d5184
eoeo0326 <eoeo0326@gmail.com>
2021-07-29 17:12:01 +0900

    NoActionBar로 수정, MyInfoActivity 추가

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MainActivity.kt
@@ -5,15 +5,19 @@ import android.os.Bundle
 import android.view.Gravity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_layout.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_menu.setOnClickListener {
             drawerLayout.openDrawer(Gravity.RIGHT)
+        }
+
+        nav_menu.getHeaderView(0).setOnClickListener {
+            startActivity<MyInfoActivity>()
         }
     }
 }
---
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/MyInfoActivity.kt
@@ -0,0 +1,27 @@
+package kr.ac.kpu.ecobasket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_my_info.*
+
+class MyInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_info)
+
+        setSupportActionBar(toolbar_myinfo)
+
+        supportActionBar?.title = ""
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
