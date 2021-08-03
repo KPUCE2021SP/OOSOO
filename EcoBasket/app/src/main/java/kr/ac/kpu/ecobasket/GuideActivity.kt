aa5918bac0947010b40a7800f64615e253437d75
eoeo0326 <eoeo0326@gmail.com>
2021-08-03 18:52:09 +0900

    가이드 페이지 Indicator 추가

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuideActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuideActivity.kt
@@ -2,8 +2,10 @@ package kr.ac.kpu.ecobasket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_guide.*
+import org.jetbrains.anko.toast
 
 class GuideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +25,31 @@ class GuideActivity : AppCompatActivity() {
                 viewpager_guide.setCurrentItem(num+1, true)
             }
         }
+
+        viewpager_guide.setPageTransformer(IndicatorPageTransformer())
     }
 
     private fun getImageList() : ArrayList<Int> {
         return arrayListOf<Int>(R.drawable.google_icon, R.drawable.kakao_icon, R.drawable.ic_launcher_foreground)
+    }
+
+    inner class IndicatorPageTransformer : ViewPager2.PageTransformer {
+        override fun transformPage(page: View, position: Float) {
+            indicator0_guide.setImageResource(R.drawable.shape_circle_gray)
+            indicator1_guide.setImageResource(R.drawable.shape_circle_gray)
+            indicator2_guide.setImageResource(R.drawable.shape_circle_gray)
+            when (viewpager_guide.currentItem) {
+                0 -> {
+                    indicator0_guide.setImageResource(R.drawable.shape_circle_green)
+                }
+                1 -> {
+                    indicator1_guide.setImageResource(R.drawable.shape_circle_green)
+                }
+                2 -> {
+                    indicator2_guide.setImageResource(R.drawable.shape_circle_green)
+                }
+            }
+        }
+
     }
 }
