5bed5ffe02b969908d33268766574c4dd98ea780
Jinho <wlsgh980118@gmail.com>
2021-08-03 23:22:20 +0900

    환경설정-회원정보-로그아웃,회원탈퇴 기능 완료 회원정보수정 기능 미구현

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/PreferencesActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/PreferencesActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_preferences.*
 import org.jetbrains.anko.alert
+import org.jetbrains.anko.customView
+import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 class PreferencesActivity : AppCompatActivity() {
@@ -16,9 +21,13 @@ class PreferencesActivity : AppCompatActivity() {
     val list_support = mutableListOf<String>()
     val list_info = mutableListOf<String>()
 
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preferences)
+
+        auth = FirebaseAuth.getInstance()
 
         setSupportActionBar(toolbar_pref)
 
@@ -31,50 +40,35 @@ class PreferencesActivity : AppCompatActivity() {
             override fun onClick(v: View, position: Int) {
                 //클릭 이벤트
                 when (position) {
-                    0 -> {
+                    0 -> { // 회원정보 전체 수정
                         toast("0번 클릭")
-                        alert(title = "회원정보 변경", message = "회원정보 변경 다이얼로그") {
-                            positiveButton("확인"){
-
-                            }
-                            negativeButton("취소"){
-
-                            }
-                        }.show()
+                        // 회원정보수정 액티비티 소환!
                     }
 
-                    1 -> {
+                    1 -> { //로그아웃하고 로그인화면으로 돌아가기
                         toast("1번 클릭")
-                        alert(title = "비밀번호 변경", message = "비밀번호 변경 다이얼로그") {
+                        alert(title = "로그아웃", message = "로그아웃 하시겠습니까?") {
                             positiveButton("확인") {
-
+                                Firebase.auth.signOut()
+                                toast("로그아웃 되었습니다.")
+                                startActivity<LoginActivity>()
+                                finish()
                             }
                             negativeButton("취소") {
-
                             }
                         }.show()
                     }
 
-                    2 -> {
+                    2 -> { //회원탈퇴하고 로그인화면으로 돌아가기
                         toast("2번 클릭")
-                        alert(title = "로그아웃", message = "로그아웃 다이얼로그") {
+                        alert(title = "회원 탈퇴", message = "회원 탈퇴 하시겠습니까?") {
                             positiveButton("확인") {
-
+                                auth.currentUser?.delete()
+                                toast("${auth.currentUser?.email} 회원탈퇴 되었습니다.")
+                                startActivity<LoginActivity>()
+                                finish()
                             }
                             negativeButton("취소") {
-
-                            }
-                        }.show()
-                    }
-
-                    3 -> {
-                        toast("3번 클릭")
-                        alert(title = "회원 탈퇴", message = "회원 탈퇴 다이얼로그") {
-                            positiveButton("확인") {
-
-                            }
-                            negativeButton("취소") {
-
                             }
                         }.show()
                     }
@@ -158,7 +152,6 @@ class PreferencesActivity : AppCompatActivity() {
 
         list_member.apply {
             add("회원정보 수정")
-            add("비밀번호 변경")
             add("로그아웃")
             add("회원 탈퇴")
         }
