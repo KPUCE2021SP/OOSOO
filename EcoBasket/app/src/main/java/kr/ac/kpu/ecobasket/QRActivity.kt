98734812c7c6a1f3664b04b25585fe3e6c5a8deb
hoho-97 <cksgh5652@gmail.com>
2021-08-16 16:41:47 +0900

    CameraX 카메라 인스턴스 추가(Realtime)

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/QRActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/QRActivity.kt
@@ -1,62 +1,127 @@
 package kr.ac.kpu.ecobasket
 
-import android.app.Activity
-import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.RadioButton
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.qr_overlay.*
-import kotlinx.android.synthetic.main.top_action_bar_in_qr.*
-import org.jetbrains.anko.toast
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class QRActivity : AppCompatActivity() {
+    private var imageCapture: ImageCapture? = null
 
-    var QRCode : String = ""    //QR코드
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qractivity)
 
-        btn_close_qr.setOnClickListener {
-            setResult(Activity.RESULT_CANCELED)
-            finish()
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        btn_test_qr.setOnClickListener { view ->
-            when(testRadioGroup.checkedRadioButtonId) {
-                R.id.radio123_123 -> {
-                    QRCode = "1a2s3d4f"
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {}
+
+    /* 뷰파인더(카메라 화면에서 미리보기) 구현 : 인스턴스 만들기 *
+     *
+     * 결과는 CameraX LifeCycle에 바인딩 됨
+     */
+    private fun startCamera() {
+        // 카메라 LifeCycle 바인딩을 위한 인스턴스
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)  //인스턴스 Get
+
+        cameraProviderFuture.addListener(Runnable {
+            // CameraX 라이프사이클 바인딩
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview : CameraX의 뷰파인더 구현 클래스
+            // Preview 객체 빌드
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
                 }
-                R.id.radio234_234 -> {
-                    QRCode = "as12df34"
-                }
-                R.id.radio777_777 -> {
-                    QRCode = "abcd1234"
-                }
+
+            // 후방 카메라로 기본 설정
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Rebinding 전에 Unbind
+                cameraProvider.unbindAll()
+
+                // 카메라 Bind하고 LifeCycle에 넘기기
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
-            setResult(Activity.RESULT_OK, intent)
-            intent.putExtra("qr", QRCode)
-            finish()
-        }
 
-        btn_inputCode.setOnClickListener {
-            /* 바코드 직접 입력 dialog 생성 */
-        }
+        }, ContextCompat.getMainExecutor(this))
+        //GetMainExecutor : Main Thread에 작동
+    }
 
-        btn_flash.setOnClickListener {
-            btn_flash?.let {
-                it.isSelected = !it.isSelected
+    //접근권한 수락 확인 함수
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    //출력 디렉토리 Get함수
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    //접근권한 반환값에 따른 결과
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
             }
         }
     }
 
-
-    /** MLkit 을 이용한 Android 내부 인식이 아닌 Firebase용 MLKit 사용
-     * 사용 로직 이해 후에 구현필요
-     */
-
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
 }
