03607d373f5e94826f495b6d60b4a4e0f678382e
eoeo0326 <eoeo0326@gmail.com>
2021-10-08 19:07:05 +0900

    가이드 이미지, 텍스트 적용

--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuideActivity.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuideActivity.kt
@@ -11,7 +11,7 @@ class GuideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guide)
 
-        val guideAdapter = GuidePagerAdapter(getImageList())
+        val guideAdapter = GuidePagerAdapter(getImageList(), getTextList())
         viewpager_guide.adapter = guideAdapter
         viewpager_guide.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
@@ -26,10 +26,56 @@ class GuideActivity : AppCompatActivity() {
         }
 
         viewpager_guide.setPageTransformer(IndicatorPageTransformer())
+
+        indicator0_guide.setOnClickListener {
+            viewpager_guide.currentItem = 0
+        }
+        indicator1_guide.setOnClickListener {
+            viewpager_guide.currentItem = 1
+        }
+        indicator2_guide.setOnClickListener {
+            viewpager_guide.currentItem = 2
+        }
+        indicator3_guide.setOnClickListener {
+            viewpager_guide.currentItem = 3
+        }
     }
 
-    private fun getImageList() : ArrayList<Int> {
-        return arrayListOf<Int>(R.drawable.google_icon, R.drawable.kakao_icon, R.drawable.ic_launcher_foreground)
+    private fun getImageList() : ArrayList<ArrayList<Int>> {
+
+        var page1 = arrayListOf<Int>(R.drawable.marker, R.drawable.qr_scan, R.drawable.eco_basket_icon, R.drawable.google_icon)
+        var page2 = arrayListOf<Int>(R.drawable.error, R.drawable.request_icon, R.drawable.solve_icon)
+        var page3 = arrayListOf<Int>(R.drawable.marker, R.drawable.qr_scan, R.drawable.return_icon, R.drawable.google_icon)
+        var page4 = arrayListOf<Int>(R.drawable.eco_point_64px, R.drawable.theme_change_icon, R.drawable.protect_icon)
+
+        return arrayListOf<ArrayList<Int>>(page1, page2, page3, page4)
+    }
+
+    private fun getTextList() : ArrayList<ArrayList<String>> {
+
+        var page1 = arrayListOf<String>(
+            "내 주변에 있는 보관함을 찾아보세요",
+            "보관함에 부착된 QR코드를 스캔합니다",
+            "보관함을 열어 장바구니를 챙겨주세요",
+            "보관함 뚜껑을 닫아주세요")
+
+        var page2 = arrayListOf<String>(
+            "장바구니를 사용하다 문제가 생겼을 시에",
+            "우측 상단 메뉴바에 환경설정-요청하기, 신고하기로 문제 상황을 알려주시면",
+            "문제를 즉각 해결해드리겠습니다")
+
+        var page3 = arrayListOf<String>(
+            "대여하신 장바구니를 들고 주변에 있는 보관함을 찾아보세요",
+            "보관함에 부착된 QR코드를 스캔합니다",
+            "보관함을 열어 장바구니를 반납해주세요",
+            "보관함 뚜껑을 닫아주세요.")
+
+        var page4 = arrayListOf<String>(
+            "정상적으로 Eco Basket을 이용해 주셨다면 에코포인트가 쌓입니다",
+            "쌓인 포인트로 다른 테마로 바꿀 수 있습니다",
+            "환경보호에 이바지해 주셔서 감사합니다")
+
+        return arrayListOf<ArrayList<String>>(page1, page2, page3, page4)
     }
 
     inner class IndicatorPageTransformer : ViewPager2.PageTransformer {
@@ -37,6 +83,7 @@ class GuideActivity : AppCompatActivity() {
             indicator0_guide.setImageResource(R.drawable.shape_circle_gray)
             indicator1_guide.setImageResource(R.drawable.shape_circle_gray)
             indicator2_guide.setImageResource(R.drawable.shape_circle_gray)
+            indicator3_guide.setImageResource(R.drawable.shape_circle_gray)
             when (viewpager_guide.currentItem) {
                 0 -> {
                     indicator0_guide.setImageResource(R.drawable.shape_circle_green)
@@ -46,6 +93,9 @@ class GuideActivity : AppCompatActivity() {
                 }
                 2 -> {
                     indicator2_guide.setImageResource(R.drawable.shape_circle_green)
+                }
+                3 -> {
+                    indicator3_guide.setImageResource(R.drawable.shape_circle_green)
                 }
             }
         }
--- EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuidePagerAdapter.kt
+++ EcoBasket/app/src/main/java/kr/ac/kpu/ecobasket/GuidePagerAdapter.kt
@@ -2,23 +2,36 @@ package kr.ac.kpu.ecobasket
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.layout_guide_image.view.*
 
-class GuidePagerAdapter(imageList: ArrayList<Int>) : RecyclerView.Adapter<GuidePagerAdapter.PagerViewHolder>() {
-    var item = imageList
+class GuidePagerAdapter(imageList: ArrayList<ArrayList<Int>>, textList: ArrayList<ArrayList<String>>) : RecyclerView.Adapter<GuidePagerAdapter.PagerViewHolder>() {
+    var images = imageList
+    var texts = textList
 
-    override fun getItemCount(): Int = item.size
+    override fun getItemCount(): Int = images.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
-        holder.image.setImageResource(item[position])
+        if (images[position].size == 3) {
+            holder.imageViews[3].isGone = true
+            holder.textViews[3].isGone = true
+        }
+
+        for (i in 0 until images[position].size) {
+            holder.imageViews[i].setImageResource(images[position][i])
+            holder.textViews[i].setText(texts[position][i])
+        }
     }
 
     inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
         (LayoutInflater.from(parent.context).inflate(R.layout.layout_guide_image, parent, false)){
 
-        val image = itemView.img_guide!!
+        val imageViews = arrayListOf<ImageView>(itemView.img_guide1, itemView.img_guide2, itemView.img_guide3, itemView.img_guide4)
+        val textViews = arrayListOf<TextView>(itemView.tv_guide1, itemView.tv_guide2, itemView.tv_guide3, itemView.tv_guide4)
     }
 }
